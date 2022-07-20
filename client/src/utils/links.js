e963777948b4091dd53f6fff9c219097b391706a
Jordi <jordi.lanau@yahoo.com>
2022-07-20 22:26:38 +0100

    Add navigation links

---
+++ client/src/utils/links.js
@@ -0,0 +1,29 @@
+const links = [
+  {
+    id: 1,
+    text: 'home',
+    path: '/',
+  },
+  {
+    id: 2,
+    text: 'products',
+    path: 'products',
+  },
+  {
+    id: 3,
+    text: 'about',
+    path: 'about',
+  },
+  {
+    id: 4,
+    text: 'contact',
+    path: 'contact',
+  },
+  {
+    id: 5,
+    text: 'account',
+    path: 'auth',
+  },
+];
+
+export default links;
