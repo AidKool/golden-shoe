a2379add7cab2b2622ecc2290ee690f085545494
Jordi <jordi.lanau@yahoo.com>
2022-07-25 21:59:45 +0100

    Add placeholder for filter button

--- client/src/components/ShoesLinksNav/index.jsx
+++ client/src/components/ShoesLinksNav/index.jsx
@@ -1,43 +1,60 @@
 import React from 'react';
-import { Stack, Typography } from '@mui/material';
-import shoesLinks from '../../utils/shoesLinks';
+import { Button, Stack, Typography } from '@mui/material';
 import { NavLink } from 'react-router-dom';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import shoesLinks from '../../utils/shoesLinks';
 
 function ShoesLinksNav({ type }) {
   return (
     <Stack
       direction="row"
-      spacing={4}
-      padding={3}
       display="flex"
+      alignItems="center"
       flexWrap="wrap"
+      justifyContent="space-between"
     >
-      {shoesLinks.map((link) => {
-        const { id, text, path } = link;
-        return (
-          <NavLink
-            key={id}
-            to={path}
-            style={{
-              textDecoration: 'none',
-            }}
-          >
-            <Typography
-              sx={{
-                color: type === text ? 'dark.main' : '#6d6d6d',
-                textDecoration: type === text ? 'underline' : 'none',
-                '&:hover': {
-                  color: 'dark.main',
-                  textDecoration: 'underline',
-                },
+      <Stack
+        direction="row"
+        spacing={4}
+        padding={3}
+        display="flex"
+        alignItems="center"
+        flexWrap="wrap"
+      >
+        {shoesLinks.map((link) => {
+          const { id, text, path } = link;
+          return (
+            <NavLink
+              key={id}
+              to={path}
+              style={{
+                textDecoration: 'none',
               }}
-              textTransform="capitalize"
             >
-              {text}
-            </Typography>
-          </NavLink>
-        );
-      })}
+              <Typography
+                sx={{
+                  color: type === text ? 'dark.main' : '#6d6d6d',
+                  textDecoration: type === text ? 'underline' : 'none',
+                  '&:hover': {
+                    color: 'dark.main',
+                    textDecoration: 'underline',
+                  },
+                }}
+                textTransform="capitalize"
+              >
+                {text}
+              </Typography>
+            </NavLink>
+          );
+        })}
+      </Stack>
+      <Button
+        variant="outlined"
+        sx={{ color: '#6d6d6d', borderColor: '#6d6d6d', marginY: 2 }}
+        endIcon={<FilterListIcon />}
+      >
+        <Typography textTransform="capitalize">filter</Typography>
+      </Button>
     </Stack>
   );
 }
